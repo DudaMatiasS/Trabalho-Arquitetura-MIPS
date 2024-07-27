0d83682bb3dbe8b3e30ae53e9c053ed28a231d2b
DudaMatiasS <membsimoess@gmail.com>
2024-07-27 10:53:43 -0300

    I added new methods for the TypeR class to know how its instruction is formed

--- src/TypeR.java
+++ src/TypeR.java
@@ -3,18 +3,29 @@ import java.util.Map;
 public class TypeR {
     String opCode;
     String shamt;
+    String funct;
+    String rs;
+    String rt;
+    String rd;
+    String[] instruction;
+    Registers registers;
     Map<String, String> typeR;
+
    public TypeR(){
        opCode="000000";
-       shamt=null;
+       shamt="00000";
+       instruction=null;
        typeR = new HashMap<>();
-       setInstructionTypeR();
+       registers= new Registers();
+       setOperatorBinary();
    }
+
     public boolean isTypeR(String mnemonic){
         return typeR.containsKey(mnemonic);
     }
-    private void setInstructionTypeR(){
-        typeR.put("add","10000");
+
+    private void setOperatorBinary(){
+        typeR.put("add","100000");
         typeR.put("addu","100001");
         typeR.put("sub","100010");
         typeR.put("subu","100011");
@@ -30,10 +41,10 @@ public class TypeR {
         typeR.put("sllv", "000100");
         typeR.put("srlv", "000110");
         typeR.put("srav", "000111");
-        typeR.put("mfhi", "000000");
-        typeR.put("mthi", "000001");
-        typeR.put("mflo", "000010");
-        typeR.put("mtlo", "000011");
+        typeR.put("mfhi", "010000");
+        typeR.put("mthi", "010001");
+        typeR.put("mflo", "010010");
+        typeR.put("mtlo", "010011");
         typeR.put("mult", "011000");
         typeR.put("multu", "011001");
         typeR.put("div", "011010");
@@ -42,23 +53,64 @@ public class TypeR {
         typeR.put("jalr", "001001");
 
     }
+   public String getFunction(String function){return typeR.getOrDefault(function, null);}
 
-   public String getFunction(String function){
-       setShamt(function);
-       return typeR.getOrDefault(function, null);
-   }
    public String getOpCode(){
        return opCode;
    }
-   public void setShamt(String function){
-       if(function.equals("sll")||function.equals("srlv")||function.equals("srl")||function.equals("sra")||function.equals("sllv")||function.equals("srav")){
-           shamt=null;
-       }else{
-           shamt="00000";
-       }
-   }
+
+   public void setShamt(String shamt){this.shamt=DecimalToBinary(shamt);}
+
    public String getShamt(){
        return shamt;
    }
+
+   public String DecimalToBinary(String decimal) {
+        int decimalNumber = Integer.parseInt(decimal);
+        String binaryString = Integer.toBinaryString(decimalNumber);
+        while (binaryString.length() < 5) {
+            binaryString = "0" + binaryString;
+        }
+        return binaryString;
+   }
+   public void setInstruction(String[] instruction){
+       this.instruction=instruction;
+       opCode= getOpCode();
+       funct = getFunction(instruction[0]);
+       setRegistersAndShamtTypeR();
+   }
+    public void setRegistersAndShamtTypeR(){
+        if(instruction[0].equals("sll")||instruction[0].equals("srl")||instruction[0].equals("sra")) {
+            rs = registers.getRegister("$zero");
+            rt = registers.getRegister(instruction[2]);
+            rd = registers.getRegister(instruction[1]);
+            setShamt(instruction[3]);
+        } else if(instruction[0].equals("mult")||instruction[0].equals("multu")||instruction[0].equals("div")||instruction[0].equals("divu")) {
+            rs = registers.getRegister(instruction[1]);
+            rt = registers.getRegister(instruction[2]);
+            rd = registers.getRegister("$zero");
+        }else if(instruction[0].equals("jalr")){
+            rs = registers.getRegister(instruction[2]);
+            rt = registers.getRegister("$zero");
+            rd = registers.getRegister(instruction[1]);
+        }else if(instruction[0].equals("mfhi")||instruction[0].equals("mflo")) {
+            rs = registers.getRegister("$zero");
+            rt = registers.getRegister("$zero");
+            rd = registers.getRegister(instruction[1]);
+        }else  if(instruction[0].equals("mthi")||instruction[0].equals("mtlo")||instruction[0].equals("jr")) {
+            rs = registers.getRegister(instruction[1]);
+            rt = registers.getRegister("$zero");
+            rd = registers.getRegister("$zero");
+        }else{
+            rs = registers.getRegister(instruction[2]);
+            rt = registers.getRegister(instruction[3]);
+            rd = registers.getRegister(instruction[1]);
+        }
+        shamt = getShamt();
+    }
+
+    public String getInstruction(){
+       return (opCode+" "+rs+" "+rt+" "+rd+" "+shamt+" "+funct);
+    }
 
 }
