154cfa8b64282caa4b1e23a061b95dade56e8552
DudaMatiasS <membsimoess@gmail.com>
2024-07-27 20:41:50 -0300

    I removed unnecessary code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Diretório de trabalho atual: " + System.getProperty("user.dir"));
         ReadAndWriteTxt reader = new ReadAndWriteTxt();
-        reader.txtReader("C:\\Users\\Duda\\Desktop\\Arquitetura\\src\\assets\\programa.txt");
+        reader.txtReader("C:\\Users\\Duda\\Desktop\\Arquitetura\\src\\assets\\program.txt");
     }
 }
