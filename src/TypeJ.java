23a465f5a7a922a94a3657c4d1e6cfe6fa4b46f7
DudaMatiasS <membsimoess@gmail.com>
2024-07-27 12:38:49 -0300

    add new Class 'TypeJ'

---
+++ src/TypeJ.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TypeJ {
+    String opCode;
+    String immediate;
+    String[] instruction;
+    Map<String, String> typeJ;
+    Tools tools;
+
+   public TypeJ(){
+       opCode=" ";
+       immediate=" ";
+       instruction=null;
+       typeJ = new HashMap<>();
+       tools=new Tools();
+       setOpCodeOperatorBinary();
+   }
+
+    public boolean isTypeJ(String mnemonic){
+        return typeJ.containsKey(mnemonic);
+    }
+
+    private void setOpCodeOperatorBinary(){
+        typeJ.put("j","000010");
+        typeJ.put("jal","000011");
+   }
+
+   public void setInstruction(String[] instruction){
+       this.instruction=instruction;
+       opCode = getOpCode(instruction[0]);
+       immediate = getImmediate();
+   }
+   public String getImmediate(){
+       return tools.DecimalToBinary(instruction[1],26);
+   }
+    public String getOpCode(String opCode){return typeJ.getOrDefault(opCode, null);}
+
+    public String getInstruction(){return (opCode+" "+immediate);}
+
+}
