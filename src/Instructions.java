f1d5b76ea498c53fc1e5c2b54acd183d4fff937b
DudaMatiasS <membsimoess@gmail.com>
2024-07-27 10:52:06 -0300

    instruction is responsible for directing instructions to their types

--- src/Instructions.java
+++ src/Instructions.java
@@ -1,48 +1,38 @@
 public class Instructions {
     String[] instruction;
-    String opcode;
-    String shamt;
-    String funct;
-    String allRegisters;
-    String immediate;
-   Registers registers;
-
     TypeR typeR;
     public Instructions(String[] instruction){
         typeR = new TypeR();
-        registers= new Registers();
+
         this.instruction = instruction;
-        if(typeR.isTypeR(instruction[0])){
-            setInstructionTypeR();
-        }
-//        else if (false/**typeI.isTypeI(instruction[0])**/) {
-//            setInstructionTypeI();
-//        } else if (false/**typeI.isTypeJ(instruction[0])**/) {
-//            setInstructionTypeJ();
-//        }
+        setInstruction();
 
     }
 //PRECISA FAZER VALIDAÇAO DO TAMANHO DA INSTRUCTION POIS SE FOR MENOR QUE 4 É NECESSARIO ADICIONAR ZERO AO FINAL DA INSTRUÇAO
-    public void setInstructionTypeR(){
-        opcode= typeR.getOpCode();
-        funct = typeR.getFunction(instruction[0]);
-        shamt= typeR.getShamt();
 
-//        for (int i=0; i<=instruction.length;i++) {
-//            if (instruction[i].startsWith("$")) {
-//                System.out.println(instruction[i]);
-//                allRegisters += registers.getRegister(instruction[i])+" ";
-//            }else{
-//                immediate = instruction[i];
-//            }
+    private void setInstruction(){
+
+        if(typeR.isTypeR(instruction[0])){
+            typeR.setInstruction(instruction);
+        }
+//        else if (false/**typeI.isTypeI(instruction[0])**/) {
+//            typeI.setInstruction();
+//        } else if (false/**typeI.isTypeJ(instruction[0])**/) {
+//            typeJ.setInstruction();
 //        }
-        System.out.println(instruction.length);
-
     }
-    public void setInstructionTypeI(){
-
+    public String getInstruction(){
+        if(typeR.isTypeR(instruction[0])){
+            return typeR.getInstruction();
+        }
+//        else if (false/**typeI.isTypeI(instruction[0])**/) {
+//            return typeI.getInstruction();
+//        } else if (false/**typeI.isTypeJ(instruction[0])**/) {
+//            return typeJ.getInstruction();
+//        }
+        return null;
     }
-    public void setInstructionTypeJ(){
 
-    }
+
+
 }
