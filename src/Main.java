b1531a9ac575e473bba367b695f47863eea425a9
PedroHL19 <pedrohlopes1903@gmail.com>
2024-07-29 19:48:44 -0300

    mudando para o caminho relativo usando Paths

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.io.*;
+import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         ReadAndWriteTxt reader = new ReadAndWriteTxt();
-        reader.txtReader("C:\\Users\\Duda\\Desktop\\Arquitetura\\src\\assets\\program.txt");
+        reader.txtReader(Paths.get("src", "assets", "program.txt").toString());
     }
 }
--- src/ReadAndWriteTxt.java
+++ src/ReadAndWriteTxt.java
@@ -1,32 +1,33 @@
 import java.io.*;
+import java.nio.file.Paths;
 
 public class ReadAndWriteTxt {
-    public void txtReader(String filePath){
+
+    public void txtReader(String filePath) {
         txtClear();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
             String line;
-            while ((line =reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 lineBreak(line.trim().toLowerCase());
             }
-
             reader.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    private void lineBreak(String line){
-        String [] caracter = line.replace("(", " ").replace(")"," ").replace(",", "").split(" ");
+
+    private void lineBreak(String line) {
+        String[] caracter = line.replace("(", " ").replace(")", " ").replace(",", "").split(" ");
         Instructions instructions = new Instructions(caracter);
         txtWriter(instructions.getBinaryInstruction());
-
     }
+
     private void txtWriter(String line) {
-        if(line == null){
+        if (line == null) {
             line = "Infelizmente não foi escrito. Verifique se há alguma instrução nessa linha! :(";
         }
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Duda\\Desktop\\Arquitetura\\src\\assets\\programOut.txt", true))) { // Modo de anexar (append)
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get("src", "assets", "programOut.txt").toString(), true))) { // Modo de anexar (append)
             writer.write(line);
             writer.newLine();
             writer.flush();
@@ -34,13 +35,13 @@ public class ReadAndWriteTxt {
             e.printStackTrace();
         }
     }
-    private void txtClear(){
+
+    private void txtClear() {
         try {
-            FileWriter fileWriter = new FileWriter("C:\\Users\\Duda\\Desktop\\Arquitetura\\src\\assets\\programOut.txt");
+            FileWriter fileWriter = new FileWriter(Paths.get("src", "assets", "programOut.txt").toString());
             fileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
