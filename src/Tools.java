73d1c122ed84e316376486336e663db6252857a1
DudaMatiasS <membsimoess@gmail.com>
2024-07-28 22:46:11 -0300

    add possibility to transform negative decimal to binary

--- src/Tools.java
+++ src/Tools.java
@@ -1,9 +1,13 @@
 public class Tools {
-    public String DecimalToBinary(String decimal,int binaryLength) {
+    public String DecimalToBinary(String decimal, int binaryLength) {
         int decimalNumber = Integer.parseInt(decimal);
-        String binaryString = Integer.toBinaryString(decimalNumber);
-        while (binaryString.length() < binaryLength) {
-            binaryString = "0" + binaryString;
+        String binaryString = Integer.toBinaryString(Integer.parseInt(decimal));
+        if (binaryString.length() > binaryLength) {
+            binaryString = binaryString.substring(binaryString.length() - binaryLength);
+        } else {
+            while (binaryString.length() < binaryLength) {
+                binaryString = ((decimalNumber < 0) ? '1' : '0') + (binaryString);
+            }
         }
         return binaryString;
     }
