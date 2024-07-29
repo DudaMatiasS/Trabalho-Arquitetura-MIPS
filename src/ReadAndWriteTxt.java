3c1c4b2a1b5010503d307b5bd9c15f332bdb72ed
DudaMatiasS <membsimoess@gmail.com>
2024-07-28 22:45:08 -0300

    add new method 'txtClear'

--- src/ReadAndWriteTxt.java
+++ src/ReadAndWriteTxt.java
@@ -2,13 +2,16 @@ import java.io.*;
 
 public class ReadAndWriteTxt {
     public void txtReader(String filePath){
+        txtClear();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
             String line;
             while ((line =reader.readLine()) != null) {
                 lineBreak(line.trim().toLowerCase());
             }
+
             reader.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -21,12 +24,20 @@ public class ReadAndWriteTxt {
     }
     private void txtWriter(String line) {
         if(line == null){
-            line = "Infelizmente não foi escrito :(";
+            line = "Infelizmente não foi escrito. Verifique se há alguma instrução nessa linha! :(";
         }
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Duda\\Desktop\\Arquitetura\\src\\assets\\programOut.txt", true))) { // Modo de anexar (append)
             writer.write(line);
             writer.newLine();
             writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void txtClear(){
+        try {
+            FileWriter fileWriter = new FileWriter("C:\\Users\\Duda\\Desktop\\Arquitetura\\src\\assets\\programOut.txt");
+            fileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
