bea8ee2bf0229bddf6551ababe68168817e3f217
Duda <duda@mobradio.com.br>
2024-09-25 14:08:48 -0300

    refactoring get and setters

--- src/Configuration.java
+++ src/Configuration.java
@@ -7,41 +7,44 @@ public class Configuration {
     int vias;
     ArrayList<Integer> address;
 
+    public int getMemorySize() {
+        return memorySize;
+    }
+
     public void setMemorySize(int memorySize) {
         this.memorySize = memorySize;
     }
 
-    
     public int getWordsLine() {
-     
-
+        return wordsLine;
     }
+
     public void setWordsLine(int wordsLine) {
-     
-
+        this.wordsLine = wordsLine;
     }
+
     public int getLines() {
-     
-
+        return lines;
     }
+
     public void setLines(int lines) {
-     
-
+        this.lines = lines;
     }
+
     public int getVias() {
-     
-
+        return vias;
     }
+
     public void setVias(int vias) {
-     
-
+        this.vias = vias;
     }
+
     public ArrayList<Integer> getAddress() {
-     
-
+        return address;
     }
-    public void setAddress(ArrayList<Integer> address) {
-     
 
+    public void setAddress(ArrayList<Integer> address) {
+        this.address = address;
+    }
 
 }
